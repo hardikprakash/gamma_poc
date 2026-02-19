dc304ffa114d3703cc7474d9538a9e0bd2bed0a1
Hardik Prakash <hardikprakash.official@gmail.com>
2026-02-19 17:27:39 +0530

    feat: update configuration for Neo4J connection and add PDF output directory

--- app/core/config.py
+++ app/core/config.py
@@ -17,10 +17,14 @@ class Settings:
         self.OPENAI_API_BASE_URL = os.getenv("OPENAI_API_BASE_URL")
         self.MODEL_NAME = os.getenv("MODEL_NAME")
 
-
         # PDF Configuration
         self.PDF_INPUT_DIR = os.getenv("PDF_INPUT_DIR", "./data")
         self.PDF_OUTPUT_DIR = os.getenv("PDF_OUTPUT_DIR", "./output")
+
+        # Neo4J
+        NEO4J_URI: str = "bolt://localhost:7687"
+        NEO4J_USER: str = "neo4j"
+        NEO4J_PASSWORD: str = "testpassword"
 
 
 settings = Settings()
